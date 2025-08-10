4e9aa770ed5da498939bdfc71bcfcc731208495e
Alexiotovv <gpsalexvasquez@gmail.com>
2025-08-09 21:01:20 -0500

    idiomas

---
+++ resources/lang/es/validation.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'attributes' => [
+        'email' => 'Correo Electrónico',
+        'password' => 'Contraseña',
+        'Login' => 'Iniciar Sesión',
+        'rememberme' => 'Recordar credenciales',
+        // Agrega más campos según necesites
+    ],
+    
+    // Mensajes de validación estándar
+    'accepted' => 'El campo :attribute debe ser aceptado.',
+    'required' => 'El campo :attribute es obligatorio.',
+    // ... otros mensajes
+];
