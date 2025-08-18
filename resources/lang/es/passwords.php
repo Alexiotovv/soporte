83b5126eedd955758ab7039cd3f77a3cac1d1ee7
Alexiotovv <gpsalexvasquez@gmail.com>
2025-08-18 18:01:21 -0500

    idioma_password

--- resources/lang/es/passwords.php
+++ resources/lang/es/passwords.php
@@ -2,7 +2,7 @@
 
 return [
     'reset' => '¡Tu contraseña ha sido restablecida!',
-    'sent' => 'Te hemos enviado por correo electrónico el enlace para restablecer tu contraseña.',
+    'sent' => 'Te hemos enviado por correo electrónico el enlace para restablecer tu contraseña. Revisa tu bandeja de entrada o en spam',
     'throttled' => 'Por favor espera antes de intentar nuevamente.',
     'token' => 'Este token de restablecimiento de contraseña no es válido.',
     'user' => "No se encontró un usuario con esa dirección de correo electrónico.",
